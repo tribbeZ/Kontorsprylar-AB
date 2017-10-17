0916c8cac9ee94fd4af2001720fb71cf1506fb62
Robin Lundquist <robin.lundquist@gmail.com>
2017-10-17 15:56:05 +0200

    Done some coding, Methods

--- SQLLibraryKAB/SQLClass.cs
+++ SQLLibraryKAB/SQLClass.cs
@@ -6,22 +6,317 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 
+
 namespace SQLLibraryKAB
 {
     public static class SQLClass
     {
-        static string connString = ""
+        static string connString = "Data Source=.;Initial Catalog=KAB;Integrated Security=True";
         static SqlConnection sqlConnection = new SqlConnection(connString);
 
-        AddCustomer();
-        AddOrder();
-        ReadAllProducts();
-        ReadAllOrders();
-        ReadStock();
-        ReadAllCustomers();
+        /// <summary>
+        /// Create customer
+        /// </summary>
+        /// <param name="firstname"></param>
+        /// <param name="lastname"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="email"></param>
+        public static void AddCustomer(string firstname, string lastname, string username, string password, string email)
+        {
 
-    
+            try
+            {
+                // öppna strömmen
+                sqlConnection.Open();
 
+                // skapa sql-kommando
+                SqlCommand mySQLCommand = new SqlCommand("AddCustomer", sqlConnection);
+
+                // säg att det är en stored procedure
+                mySQLCommand.CommandType = CommandType.StoredProcedure;
+
+                // skapa parametrar
+                SqlParameter paramFirstname = new SqlParameter("@firstname", SqlDbType.VarChar);
+                paramFirstname.Value = firstname;
+
+                SqlParameter paramLastname = new SqlParameter("@lastname", SqlDbType.VarChar);
+                paramLastname.Value = lastname;
+
+                SqlParameter paramUsername = new SqlParameter("@username", SqlDbType.VarChar);
+                paramUsername.Value = username;
+
+                SqlParameter paramPassword = new SqlParameter("@password", SqlDbType.VarChar);
+                paramPassword.Value = password;
+
+                SqlParameter paramEmail = new SqlParameter("@email", SqlDbType.VarChar);
+                paramEmail.Value = email;
+
+                SqlParameter paramCID = new SqlParameter("@cid", SqlDbType.Int);
+                paramCID.Direction = ParameterDirection.Output;
+
+                // lägg till parametrar
+                mySQLCommand.Parameters.Add(paramFirstname);
+                mySQLCommand.Parameters.Add(paramLastname);
+                mySQLCommand.Parameters.Add(paramUsername);
+                mySQLCommand.Parameters.Add(paramPassword);
+                mySQLCommand.Parameters.Add(paramEmail);
+                mySQLCommand.Parameters.Add(paramCID);
+
+
+
+                mySQLCommand.ExecuteNonQuery();
+
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Add product
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="price"></param>
+        public static void AddProduct(string name, string price)
+        {
+            try
+            {
+                sqlConnection.Open();
+
+                // skapa sql-kommando
+                SqlCommand mySQLCommand = new SqlCommand("CreateProducts", sqlConnection);
+
+                // säg att det är en stored procedure
+                mySQLCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramName = new SqlParameter("@name", SqlDbType.VarChar);
+                paramName.Value = name;
+
+                SqlParameter paramPrice = new SqlParameter("@price", SqlDbType.VarChar);
+                paramPrice.Value = price;
+
+                SqlParameter paramPID = new SqlParameter("@pid", SqlDbType.Int);
+                paramPID.Direction = ParameterDirection.Output;
+
+                mySQLCommand.Parameters.Add(paramName);
+                mySQLCommand.Parameters.Add(paramPrice);
+                mySQLCommand.Parameters.Add(paramPID);
+
+                mySQLCommand.ExecuteNonQuery();
+
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Update customer password
+        /// </summary>
+        /// <param name="cid"></param>
+        /// <param name="password"></param>
+        public static void UpdateCustomer(int cid, string password)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("UpdateCustomer", sqlConnection);
+
+                sqlCommand.CommandType = CommandType.StoredProcedure; //vår commandtype är en stored procedure
+
+                SqlParameter paramCID = new SqlParameter("@cid", SqlDbType.Int);
+                paramCID.Value = cid;
+
+                SqlParameter paramPass = new SqlParameter("@password", SqlDbType.VarChar);
+                paramPass.Value = password;
+
+                sqlCommand.Parameters.Add(paramCID);
+                sqlCommand.Parameters.Add(paramPass);
+
+                sqlCommand.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Add to stock
+        /// </summary>
+        /// <param name="numberOfProducts"></param>
+        /// <param name="pid"></param>
+        public static void AddStockEntry(int numberOfProducts, int pid)
+        {
+            try
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("CreateStockEntry", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramSID = new SqlParameter("@sid", SqlDbType.Int);
+                paramSID.Direction = ParameterDirection.Output;
+
+                SqlParameter paramPID = new SqlParameter("@pid", SqlDbType.Int);
+                paramPID.Value = pid;
+
+                SqlParameter paramNOP = new SqlParameter("@numberofproducts", SqlDbType.Int);
+                paramNOP.Value = numberOfProducts;
+
+                sqlCommand.Parameters.Add(paramSID);
+                sqlCommand.Parameters.Add(paramPID);
+                sqlCommand.Parameters.Add(paramNOP);
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Update product
+        /// </summary>
+        /// <param name="pid"></param>
+        /// <param name="name"></param>
+        /// <param name="price"></param>
+        public static void UpdateProduct(int pid, string name, string price)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("UpdateProduct", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramPID = new SqlParameter("@pid", SqlDbType.Int);
+                paramPID.Value = pid;
+
+                SqlParameter paramName = new SqlParameter("@name", SqlDbType.VarChar);
+                paramName.Value = name;
+
+                SqlParameter paramPrice = new SqlParameter("@price", SqlDbType.VarChar);
+                paramPrice.Value = price;
+
+                sqlCommand.Parameters.Add(paramPID);
+                sqlCommand.Parameters.Add(paramName);
+                sqlCommand.Parameters.Add(paramPrice);
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Updating stock
+        /// </summary>
+        /// <param name="sid"></param>
+        /// <param name="numberofproducts"></param>
+        public static void UpdateStock(int sid, int numberofproducts)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("UpdateStock", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramSID = new SqlParameter("@sid", SqlDbType.Int);
+                paramSID.Value = sid;
+
+                SqlParameter paramNRofProducts = new SqlParameter("@numberofproducts", SqlDbType.Int);
+                paramNRofProducts.Value = numberofproducts;
+
+                sqlCommand.Parameters.Add(paramSID);
+                sqlCommand.Parameters.Add(paramNRofProducts);
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Delete contact by contact id
+        /// </summary>
+        /// <param name="pid"></param>
+        public static void DeleteProduct(int pid)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("DeleteProduct", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramPID = new SqlParameter("@pid", SqlDbType.Int);
+                paramPID.Value = pid;
+
+                sqlCommand.Parameters.Add(paramPID);
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+        }
 
     }
 }

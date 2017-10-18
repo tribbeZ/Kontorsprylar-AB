0ab76d919270be514185a22f1a04f62e08b84a25
Robin Lundquist <robin.lundquist@gmail.com>
2017-10-18 13:15:01 +0200

    nu kan man logga in baby

--- KontorsprylarAB/index.aspx.designer.cs
+++ KontorsprylarAB/index.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace KontorsprylarAB {
     
     
     public partial class index {
+        
+        /// <summary>
+        /// labelUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelUser;
     }
 }
